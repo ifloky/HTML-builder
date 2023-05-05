01f80f6058b4439e9fb59dd326c3e88950d8a671
iflocky <Petrenko1702@gmail.com>
2023-05-05 15:57:48 +0300

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,55 +1,31 @@
-
 const fs = require('fs');
 const path = require('path');
 
-async function copyDir() {
-  const srcDir = path.join(__dirname, 'files');
-  const destDir = path.join(__dirname, 'files-copy');
-  try {
-    const dirExists = await fs.promises.stat(destDir);
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      await fs.promises.mkdir(destDir, { recursive: true });
-    } else {
-      console.error(error);
-    }
-  }
-  const files = await fs.promises.readdir(srcDir, { withFileTypes: true });
-  await Promise.all(
-    files.map(async (file) => {
-      const srcPath = path.join(srcDir, file.name);
-      const destPath = path.join(destDir, file.name);
-      if (file.isFile()) {
-        await fs.promises.copyFile(srcPath, destPath);
-      } else if (file.isDirectory()) {
-        await copyDirRecursive(srcPath, destPath);
-      }
-    })
-  );
-}
+const srcPath = path.join(__dirname, 'files');
+const destPath = path.join(__dirname, 'files-copy');
 
-async function copyDirRecursive(srcDir, destDir) {
+async function copyDir() {
   try {
-    const dirExists = await fs.promises.stat(destDir);
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      await fs.promises.mkdir(destDir, { recursive: true });
-    } else {
-      console.error(error);
-    }
-  }
-  const files = await fs.promises.readdir(srcDir, { withFileTypes: true });
-  await Promise.all(
-    files.map(async (file) => {
-      const srcPath = path.join(srcDir, file.name);
-      const destPath = path.join(destDir, file.name);
-      if (file.isFile()) {
-        await fs.promises.copyFile(srcPath, destPath);
-      } else if (file.isDirectory()) {
-        await copyDirRecursive(srcPath, destPath);
+    await fs.promises.mkdir(destPath, { recursive: true });
+
+    const files = await fs.promises.readdir(srcPath);
+
+    for (const file of files) {
+      const srcFile = path.join(srcPath, file);
+      const destFile = path.join(destPath, file);
+      const stat = await fs.promises.stat(srcFile);
+      if (stat.isDirectory()) {
+        await copyDir(srcFile, destFile);
+      } else {
+        const readStream = fs.createReadStream(srcFile);
+        const writeStream = fs.createWriteStream(destFile);
+        readStream.pipe(writeStream);
       }
-    })
-  );
+    }
+    console.log('Копирование завершено!');
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 copyDir();
