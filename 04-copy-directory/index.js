5fc0f6a22969e326977c634948e850e8583c5ee0
iflocky <Petrenko1702@gmail.com>
2023-05-05 15:55:05 +0300

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,55 @@
 
+const fs = require('fs');
+const path = require('path');
+
+async function copyDir() {
+  const srcDir = path.join(__dirname, 'files');
+  const destDir = path.join(__dirname, 'files-copy');
+  try {
+    const dirExists = await fs.promises.stat(destDir);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.promises.mkdir(destDir, { recursive: true });
+    } else {
+      console.error(error);
+    }
+  }
+  const files = await fs.promises.readdir(srcDir, { withFileTypes: true });
+  await Promise.all(
+    files.map(async (file) => {
+      const srcPath = path.join(srcDir, file.name);
+      const destPath = path.join(destDir, file.name);
+      if (file.isFile()) {
+        await fs.promises.copyFile(srcPath, destPath);
+      } else if (file.isDirectory()) {
+        await copyDirRecursive(srcPath, destPath);
+      }
+    })
+  );
+}
+
+async function copyDirRecursive(srcDir, destDir) {
+  try {
+    const dirExists = await fs.promises.stat(destDir);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.promises.mkdir(destDir, { recursive: true });
+    } else {
+      console.error(error);
+    }
+  }
+  const files = await fs.promises.readdir(srcDir, { withFileTypes: true });
+  await Promise.all(
+    files.map(async (file) => {
+      const srcPath = path.join(srcDir, file.name);
+      const destPath = path.join(destDir, file.name);
+      if (file.isFile()) {
+        await fs.promises.copyFile(srcPath, destPath);
+      } else if (file.isDirectory()) {
+        await copyDirRecursive(srcPath, destPath);
+      }
+    })
+  );
+}
+
+copyDir();
