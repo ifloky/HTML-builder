5a39c2f727a96dd15fb11fe107b11522e3201b60
iflocky <Petrenko1702@gmail.com>
2023-05-10 09:35:39 +0300

    fix: fix bugs

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,6 +3,15 @@ const path = require('path');
 
 const stylesDirPath = path.join(__dirname, 'styles');
 const bundleFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
+const projectDistPath = path.join(__dirname, 'project-dist');
+
+if (!fs.existsSync(projectDistPath)) {
+  fs.mkdir(projectDistPath, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
 
 fs.readdir(stylesDirPath, (err, files) => {
   if (err) {
@@ -12,11 +21,25 @@ fs.readdir(stylesDirPath, (err, files) => {
 
   const cssFiles = files.filter((file) => path.extname(file) === '.css');
 
-  const styles = cssFiles.map((file) =>
-    fs.readFileSync(path.join(stylesDirPath, file)).toString()
-  );
+  const styles = [];
 
-  fs.writeFileSync(bundleFilePath, styles.join('\n'));
-  
-  console.log(`File bundle.css has been successfully created in project-dist folder.`);
+  cssFiles.forEach((file, index) => {
+    fs.readFile(path.join(stylesDirPath, file), (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      styles[index] = data.toString();
+
+      if (index === cssFiles.length - 1) {
+        fs.writeFile(bundleFilePath, styles.join('\n'), (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log(`File bundle.css has been successfully created in project-dist folder.`);
+        });
+      }
+    });
+  });
 });
